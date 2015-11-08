dc0f45b958b35d4af0eb8315531d24f7a7b7b11f
ElaineMou <elaine-mou@uiowa.edu>
2015-11-08 01:38:41 -0500

    Most recent stuff

--- app/src/main/java/devilhawks/hackduke/hackdukeproject/FaceGraphic.java
+++ app/src/main/java/devilhawks/hackduke/hackdukeproject/FaceGraphic.java
@@ -101,32 +101,35 @@ class FaceGraphic extends GraphicOverlay.Graphic {
         // Draws a circle at the position of the detected face, with the face's track id below.
         float x = translateX(face.getPosition().x + face.getWidth() / 2);
         float y = translateY(face.getPosition().y + face.getHeight() / 2);
-        canvas.drawCircle(x, y, FACE_POSITION_RADIUS, mFacePositionPaint);
+
+        float leftEyeX=0, leftEyeY=0, rightEyeX=0, rightEyeY =0;
+        List<Landmark> landmarks = face.getLandmarks();
+        for(Landmark landmark : landmarks){
+            int type = landmark.getType();
+            if(type == Landmark.RIGHT_EYE){
+                rightEyeX = landmark.getPosition().x;
+                rightEyeY = landmark.getPosition().y;
+            } else if (type == Landmark.LEFT_EYE){
+                leftEyeX = landmark.getPosition().x;
+                leftEyeY = landmark.getPosition().y;
+            }
+        }
+
+        canvas.drawText("right eye: " + String.format("%.2f", rightEyeX) + " " + String.format("%.2f", rightEyeY), x + ID_X_OFFSET * 2, y + ID_Y_OFFSET * 2, mIdPaint);
+        canvas.drawText("left eye: " + String.format("%.2f", leftEyeX) + " " + String.format("%.2f", leftEyeY), x - ID_X_OFFSET*2, y - ID_Y_OFFSET*2, mIdPaint);
+        //canvas.drawCircle(x, y, FACE_POSITION_RADIUS, mFacePositionPaint);
         //canvas.drawText("id: " + mFaceId, x + ID_X_OFFSET, y + ID_Y_OFFSET, mIdPaint);
         //canvas.drawText("happiness: " + String.format("%.2f", face.getIsSmilingProbability()), x - ID_X_OFFSET, y - ID_Y_OFFSET, mIdPaint);
         //canvas.drawText("right eye: " + String.format("%.2f", face.getIsRightEyeOpenProbability()), x + ID_X_OFFSET * 2, y + ID_Y_OFFSET * 2, mIdPaint);
         //canvas.drawText("left eye: " + String.format("%.2f", face.getIsLeftEyeOpenProbability()), x - ID_X_OFFSET*2, y - ID_Y_OFFSET*2, mIdPaint);
-        canvas.drawText("Y: " + String.format("%.2f", face.getEulerY()) + "\nZ: " + String.format("%.2f", face.getEulerZ()), x - ID_X_OFFSET*2, y - ID_Y_OFFSET*2, mIdPaint);
+        //canvas.drawText("Y: " + String.format("%.2f", face.getEulerY()) + "\nZ: " + String.format("%.2f", face.getEulerZ()), x - ID_X_OFFSET*2, y - ID_Y_OFFSET*2, mIdPaint);
         // Draws a bounding box around the face.
-        float xOffset = scaleX(face.getWidth() / 2.0f);
+        /*float xOffset = scaleX(face.getWidth() / 2.0f);
         float yOffset = scaleY(face.getHeight() / 2.0f);
         float left = x - xOffset;
         float top = y - yOffset;
         float right = x + xOffset;
         float bottom = y + yOffset;
-        canvas.drawRect(left, top, right, bottom, mBoxPaint);
-
-        Paint paint = new Paint();
-        paint.setColor(Color.GREEN);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(5);
-
-        List<Landmark> landmarks = face.getLandmarks();
-
-        for (Landmark landmark : landmarks) {
-            int cx = (int)translateX (landmark.getPosition().x);
-            int cy = (int) translateY(landmark.getPosition().y);
-            canvas.drawCircle(cx, cy, 10, paint);
-        }
+        canvas.drawRect(left, top, right, bottom, mBoxPaint);*/
     }
 }
